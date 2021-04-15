8ed33079712ac5786e6fcc64032ea05f0325e88b
Andreas Hahn <andreas.hahn@dataformers.at>
2021-04-15 22:18:55 +0200

    Added blazor client with social login and aspnetcore identity

---
+++ src/Web/CleanArchitecture.Web.Blazor/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,57 @@
+﻿using CleanArchitecture.Application;
+using CleanArchitecture.Application.Configurations;
+using CleanArchitecture.Infrastructure;
+using CleanArchitecture.Infrastructure.Database.Identity;
+using CleanArchitecture.Infrastructure.Services.AzureStorage;
+using CleanArchitecture.Infrastructure.Services.Email;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Web.Blazor.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.RegisterInfrastructure(configuration);
+            services.RegisterCore();
+
+            services.ConfigureAppSettings(configuration);
+
+            return services;
+        }
+
+        private static void ConfigureAppSettings(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<AuthenticationConfiguration>(configuration.GetSection("Authentication"));
+            services.Configure<AzureStorageConfiguration>(configuration.GetSection("AzureStorage"));
+            services.Configure<SendGridConfiguration>(configuration.GetSection("SendGrid"));
+        }
+
+        public static void AddAuthenticationServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services
+                .AddIdentity<IdentityUser, IdentityRole>()
+                .AddDefaultUI()
+                .AddEntityFrameworkStores<IdentityContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
+
+            services
+                .AddAuthentication(AzureADB2CDefaults.AuthenticationScheme)
+                .AddAzureADB2C(options => configuration.Bind("Authentication:AzureAdB2C", options))
+                .AddGoogle(options => configuration.Bind("Authentication:Google", options))
+                .AddFacebook(options => configuration.Bind("Authentication:Facebook", options));
+        }
+    }
+}
